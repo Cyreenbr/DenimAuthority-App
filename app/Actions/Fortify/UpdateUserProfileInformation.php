e5f60be030adfc8eca726335d6838d24088cca7e
Cyreenbr <dallelis98@gmail.com>
2023-04-18 23:41:04 +0100

    profile

--- app/Actions/Fortify/UpdateUserProfileInformation.php
+++ app/Actions/Fortify/UpdateUserProfileInformation.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 use Laravel\Fortify\Contracts\UpdatesUserProfileInformation;
+use Illuminate\Http\RedirectResponse;
 
 class UpdateUserProfileInformation implements UpdatesUserProfileInformation
 {
@@ -37,6 +38,8 @@ class UpdateUserProfileInformation implements UpdatesUserProfileInformation
                 'email' => $input['email'],
             ])->save();
         }
+
+        return redirect()->route('welcome');
     }
 
     /**
