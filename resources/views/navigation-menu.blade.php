99dd875a81d3c7254b696a3e775f2e814ab63516
Cyreenbr <dallelis98@gmail.com>
2023-04-18 23:48:29 +0100

    last profile modif

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -5,17 +5,11 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto" />
+                    <a href="{{ route('welcome') }}">
+            <img src="/assets/images/DA-logo.png" style="height:50px;width:70px" alt="logo" />
                     </a>
                 </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-jet-nav-link>
-                </div>
+               
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
