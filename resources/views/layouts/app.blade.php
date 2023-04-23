051b95b6b24d582256bbc9e611eb24f921680236
Cyreenbr <dallelis98@gmail.com>
2023-04-23 14:05:13 +0100

    title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Denim Authority Dashboard') }}</title>
-
+        <title>Denim Authority Dashboard</title>
+        <link rel="shortcut icon" href="/assets/images/DA-logo.png" />
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
